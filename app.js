fce4b7e2f54b6f72db86f2220ae0822e8ee79595
Essien Emmanuel Samuel <essienemma300@gmail.com>
2022-10-09 18:57:58 +0100

    added error page

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const tableSync = async () => {
 // apiCall();
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found!');
+    res.render('error');
 });
 
 app.listen(port, () => {
